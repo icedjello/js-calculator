b1069ab90cc3ee83d1fff531791640c35eae519e
icedjello <bamfard@gmail.com>
2020-12-20 17:34:22 +0000

    Added buttons and button  listeners.

---
+++ main.js
@@ -0,0 +1,19 @@
+const PLUS_BUTTON = document.getElementById('plusButton');
+const MINUS_BUTTON = document.getElementById('minusButton');
+const MULTIPLY_BUTTON = document.getElementById('multiplyButton');
+const DIVIDE_BUTTON = document.getElementById('divideButton');
+const EQUALS_BUTTON = document.getElementById('equalsButton');
+
+const ONE_BUTTON =document.getElementById('oneButton');
+const TWO_BUTTON = document.getElementById('twoButton');
+const THREE_BUTTON = document.getElementById('threeButton');
+const FOUR_BUTTON = document.getElementById('fourButton');
+const FIVE_BUTTON = document.getElementById('fiveButton');
+const SIX_BUTTON = document.getElementById('sixButton');
+const SEVEN_BUTTON = document.getElementById('sevenButton');
+const EIGHT_BUTTON = document.getElementById('eightButton');
+const NINE_BUTTON = document.getElementById('nineButton');
+const ZERO_BUTTON = document.getElementById('zeroButton');
+
+
+
